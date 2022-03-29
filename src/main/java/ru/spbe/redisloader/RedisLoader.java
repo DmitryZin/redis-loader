936b0849d8c0f4cf1278fe25789f3dc253a1fcb3
Dmitry Zinovev <zinovevdmv@gmail.com>
2022-03-29 17:52:42 +0300

    рефакторинг

--- src/main/java/ru/spbe/redisloader/RedisLoader.java
+++ src/main/java/ru/spbe/redisloader/RedisLoader.java
@@ -3,8 +3,8 @@ package ru.spbe.redisloader;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import ru.spbe.redisstarter.IRedisSet;
 import ru.spbe.redisstarter.Redis;
+import ru.spbe.redisstarter.RedisSet;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -13,11 +13,12 @@ import java.util.Set;
 
 @RequiredArgsConstructor
 public class RedisLoader<T> {
+    private final Class<? extends T> tClass;
     private final Redis redis;
-    private final IRedisSet set;
+    private final RedisSet set;
     private final ObjectMapper mapper;
 
-    public Set<T> getValues(Class<T> tClass) throws JsonProcessingException {
+    public Set<T> getValues() throws JsonProcessingException {
         java.util.Set<T> result = new HashSet<>();
         Set<String> setValues = redis.getDataSet(set.getDataSetName());
         for(String value : setValues){
@@ -26,7 +27,7 @@ public class RedisLoader<T> {
         return result;
     }
 
-    public List<T> getListValues(Class<T> tClass) throws JsonProcessingException {
+    public List<T> getListValues() throws JsonProcessingException {
         List<T> result = new ArrayList<>();
         Set<String> setValues = redis.getDataSet(set.getDataSetName());
         for(String value : setValues){
