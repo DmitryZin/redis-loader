898b6fe77053dce7978661e2b826abd0d9cdd268
Dmitry Zinovev <zinovevdmv@gmail.com>
2022-03-30 16:45:01 +0300

    рефакторинг, все проще

--- src/main/java/ru/spbe/redisloader/RedisLoader.java
+++ src/main/java/ru/spbe/redisloader/RedisLoader.java
@@ -3,14 +3,24 @@ package ru.spbe.redisloader;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import redis.clients.jedis.JedisPubSub;
 import ru.spbe.redisstarter.Redis;
 import ru.spbe.redisstarter.RedisSet;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+/**
+ *  Класс {@link RedisLoader} реализует чтение данных из БД Redis
+ *
+ *  * Требует  {@link Redis} - подключение к БД
+ *  * Требует  {@link RedisSet} - описание хранения данных
+ *  * Требует  {@link ObjectMapper} - для сериализации, чтоб не создавать свой экземпляр для каждого класса
+ *  * также в конструкторе требуется указать класс для десиарилации
+ */
+@Slf4j
 @RequiredArgsConstructor
 public class RedisLoader<T> {
     private final Class<? extends T> tClass;
@@ -18,14 +28,8 @@ public class RedisLoader<T> {
     private final RedisSet set;
     private final ObjectMapper mapper;
 
-    public Set<T> getValues() throws JsonProcessingException {
-        java.util.Set<T> result = new HashSet<>();
-        Set<String> setValues = redis.getDataSet(set.getDataSetName());
-        for(String value : setValues){
-            result.add(mapper.readValue(value, tClass));
-        }
-        return result;
-    }
+    private JedisPubSub jedisPubSub;
+    private List<T> list;
 
     public List<T> getListValues() throws JsonProcessingException {
         List<T> result = new ArrayList<>();
@@ -34,5 +38,39 @@ public class RedisLoader<T> {
             result.add(mapper.readValue(value, tClass));
         }
         return result;
+    }
+
+    public void unSubscribe(){
+        redis.unSubscribe(jedisPubSub);
+        jedisPubSub = null;
+    }
+
+    public void reLoad() throws JsonProcessingException {
+        if(list == null)
+            return;
+        list.clear();
+        list.addAll(getListValues());
+    }
+    /**
+     * Подписаться на сообщения - создать "слушателя"
+     */
+    public void subscribe(List<T> list){
+        this.list = list;
+        if(jedisPubSub != null){
+            log.info("I'm already subscribe");
+            return;
+        }
+        jedisPubSub = new JedisPubSub(){
+            @Override
+            public void onMessage(String channel, String message){
+                try {
+                    reLoad();
+                } catch (JsonProcessingException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        redis.subscribe(jedisPubSub, set.getChannelName());
     }
 }
