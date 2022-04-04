2ca637d03c82708f0c03a8895f3b4fc40f8b27a0
Dmitry Zinovev <zinovevdmv@gmail.com>
2022-04-05 02:13:34 +0300

    небольшая красота

--- src/main/java/ru/spbe/redisloader/RedisMapLoader.java
+++ src/main/java/ru/spbe/redisloader/RedisMapLoader.java
@@ -7,30 +7,30 @@ import ru.spbe.redisstarter.RedisSet;
 import java.util.HashMap;
 import java.util.Map;
 
-public class RedisMapLoader<K, T> extends RedisLoader{
+public class RedisMapLoader<K, V> extends RedisLoader{
 
-    private final Class<? extends T> tClass;
     private final Class<? extends K> kClass;
+    private final Class<? extends V> vClass;
 
-    private Map<K, T> map;
+    private Map<K, V> map;
 
-    public RedisMapLoader(Class<? extends K> kClass, Class<? extends T> tClass, String host, int port, RedisSet set, ObjectMapper mapper, boolean isSubscriber) throws JsonProcessingException {
+    public RedisMapLoader(Class<? extends K> kClass, Class<? extends V> vClass, String host, int port, RedisSet set, ObjectMapper mapper, boolean isSubscriber) throws JsonProcessingException {
         super(host, port, set, mapper, isSubscriber);
         this.kClass = kClass;
-        this.tClass = tClass;
+        this.vClass = vClass;
         reLoad();
     }
 
-    public Map<K, T> getValues(){
+    public Map<K, V> getValues(){
         return new HashMap<>(map);
     }
 
     @Override
     public void reLoad() throws JsonProcessingException {
-        Map<K, T> result = new HashMap<>();
+        Map<K, V> result = new HashMap<>();
         Map<String, String> keys = redis.getKeyDataSet(set.getDataSetName());
         for(Map.Entry<String, String> entry : keys.entrySet()){
-            result.put(mapper.readValue(entry.getKey(), kClass), mapper.readValue(entry.getValue(), tClass));
+            result.put(mapper.readValue(entry.getKey(), kClass), mapper.readValue(entry.getValue(), vClass));
         }
         map = result;
     }
