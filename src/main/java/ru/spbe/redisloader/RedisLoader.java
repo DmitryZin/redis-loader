f9ed63c219b8ca8ef7d734cc9ea17a841af526e9
Dmitry Zinovev <zinovevdmv@gmail.com>
2022-04-05 03:24:57 +0300

    рефакторинг. Только справочник key/value

--- src/main/java/ru/spbe/redisloader/RedisLoader.java
+++ src/main/java/ru/spbe/redisloader/RedisLoader.java
@@ -7,21 +7,53 @@ import redis.clients.jedis.JedisPubSub;
 import ru.spbe.redisstarter.Redis;
 import ru.spbe.redisstarter.RedisSet;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Slf4j
-public abstract class RedisLoader {
-    final RedisSet set;
-    final ObjectMapper mapper;
+public class RedisLoader <K, V> {
+    private final RedisSet set;
+    private final ObjectMapper mapper;
+    private final Redis redis;
+    private JedisPubSub jedisPubSub;
 
-    JedisPubSub jedisPubSub;
-    Redis redis;
+    private final Class<? extends K> kClass;
+    private final Class<? extends V> vClass;
 
-    protected RedisLoader(String host, int port, RedisSet set, ObjectMapper mapper, boolean isSubscriber) throws JsonProcessingException {
+    private Map<K, V> map;
+
+    private boolean isStateReload = false;
+
+    public RedisLoader(Class<? extends K> kClass, Class<? extends V> vClass, String host, int port, RedisSet set, ObjectMapper mapper, boolean isSubscriber) throws JsonProcessingException {
+        this.kClass = kClass;
+        this.vClass = vClass;
         this.set = set;
         this.mapper = mapper;
         redis = new Redis(host, port);
+        reLoad();
         if(isSubscriber){
             subscribe();
         }
+    }
+
+    public void reLoad() throws JsonProcessingException {
+        if(isStateReload)
+            return;
+        isStateReload = true;
+        try {
+            Map<K, V> result = new HashMap<>();
+            Map<String, String> keys = redis.getKeyDataSet(set.getDataSetName());
+            for (Map.Entry<String, String> entry : keys.entrySet()) {
+                result.put(mapper.readValue(entry.getKey(), kClass), mapper.readValue(entry.getValue(), vClass));
+            }
+            map = result;
+        } finally {
+            isStateReload = false;
+        }
+    }
+
+    public Map<K, V> getValues(){
+        return new HashMap<>(map);
     }
 
     public void unSubscribe(){
@@ -50,5 +82,4 @@ public abstract class RedisLoader {
         redis.subscribe(jedisPubSub, set.getChannelName());
     }
 
-    public abstract void reLoad() throws JsonProcessingException;
 }
